fbd66b485d5f4292abf0dfa303ef76589c56f08d
danger1406 <fric3089@gmail.com>
2025-06-19 20:04:54 +0530

    Traing of the model

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,16 @@
 import os
 import sys
-from src.exception import CustomException
+from src.execption import CustomException
 from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
+#Inputs to data ingestion
 @dataclass
 class DataIngestionConfig:
     #The artifacts folder is used to store the train outputs. 
@@ -46,6 +51,10 @@ class DataIngestion:
             
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
-    
-        
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.intitiate_model_trainer(train_array=train_arr,test_array=test_arr))
+ 
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,1 +1,74 @@
 #Components are the modules that we are specifically going to use in the project. 
+
+import os
+import sys
+from dataclasses import dataclass
+# For every file we will write a config file so that we can create the paths that are needed
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (AdaBoostClassifier,GradientBoostingClassifier,RandomForestRegressor)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.execption import CustomException
+from src.logger import logging
+
+from src.utils import save_object,evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+        
+    def intitiate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("Splitting Training and testing data")
+            # Xtrain and y train lo combine aii untai dependend and indepen features 
+            x_train,x_test,y_train,y_test=(
+                train_array[:,:-1],
+                test_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,-1]
+            )
+            models = {
+            "Linear Regression": LinearRegression(),
+            "K-Neighbors Regressor": KNeighborsRegressor(),
+            "Decision Tree": DecisionTreeRegressor(),
+            "Random Forest Regressor": RandomForestRegressor(),
+            "XGBRegressor": XGBRegressor(), 
+            "CatBoosting Regressor": CatBoostRegressor(verbose=False),  # 'verbose' controls the amount of logging output during training. Setting verbose=False disables training logs.
+            }
+            # Now we will create a function that evaluates the data for each model and gives the ouput 
+            
+            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+            # The function will be created in utils file
+            
+            # We will take the best score of all the models that we have tested 
+            best_model_score=max(sorted(model_report.values()))
+            
+            # To ge the name of the model that had got this score using the index method 
+            best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            
+            best_model=models[best_model_name]
+            
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info("Completed training and testing of data and found the model")
+            
+            save_object(
+                file_path=ModelTrainerConfig.trained_model_file_path,
+                obj=best_model
+            )
+            
+            predicted=best_model.predict(x_test)
+            model_r2_score=r2_score(y_test,predicted)
+            return model_r2_score
+        except Exception as e:
+            raise CustomException(e,sys)
+            
+        
