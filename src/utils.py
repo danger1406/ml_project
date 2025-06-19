56afbdde8d22a64814358d5e550dacb71a39a3cd
danger1406 <fric3089@gmail.com>
2025-06-19 20:30:58 +0530

    Hyper paprameter tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
 import dill
+from sklearn.model_selection import GridSearchCV
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -18,11 +20,17 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
         for i in range(len(list(models.values()))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            gs =GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+            
+            # After doing a grid search CV, you will get the optimal parameters. We will use those parameters for the model so that they give the most accurate result.
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
             
             y_train_pred=model.predict(x_train)
